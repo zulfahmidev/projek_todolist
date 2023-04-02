02002d7e46151cec58cba16fd25dca1d2c143f1c
zulfahmidev <developer.zulfahmi@gmail.com>
2023-04-03 00:58:12 +0700

    repair

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'barehbut',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
-      script: './node_modules/nuxt/bin/nuxt.js',
+      script: './node_modules/nuxt/bin/nuxt.mjs',
       args: 'start'
     }
   ]
